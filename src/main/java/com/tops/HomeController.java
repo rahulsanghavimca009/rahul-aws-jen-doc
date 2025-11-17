5ce27ccaed0996056378d61197ed7b02569b184d
Rahul Git <49506612+rahulsanghavimca009@users.noreply.github.com>
2025-11-17 23:33:30 +0530

    Update HomeController.java

--- src/main/java/com/tops/HomeController.java
+++ src/main/java/com/tops/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController 
 {
-	@GetMapping("home")
+	@GetMapping("/home")
 	public 	ResponseEntity<String> home()
 	{
 		return ResponseEntity.ok("Welcome to Deploy Project by Rahul Sanghavi AWS EC2 Git Jenkins Docker");
